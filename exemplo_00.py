b33a96294cb4b70c4ba16d76e902c4d1b42dd452
CamilaFre <camila.frech@hotmail.com>
2024-10-12 14:52:27 -0300

    salvando alteração

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,1 +1,12 @@
-print("Hello Wordl!")
+#Crie programa que usuário digita o seu nome retorna o tamanho
+
+nome = input("Digite seu nome")
+tamanho = len(nome)
+print(f'{nome}, seu nome possui {tamanho} letras!')
+
+
+#Crie um programa crie digite 2 valores e retorna a soma
+n1 = int(input('Digite o primeiro número:'))
+n2 = int(input ('Digite o segundo número:'))
+soma = n1+n2
+print(soma)
